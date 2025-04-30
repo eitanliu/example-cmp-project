136d0732914fabf11bb866b960578baa79742467
eitanliu <eitanliu@gmail.com>
2025-05-01 02:55:50 +0800

    cinterop

--- composeApp/src/nativeInterop/cinterop/primelib.c
+++ composeApp/src/nativeInterop/cinterop/primelib.c
@@ -1,19 +1,19 @@
-//
-//
-//char* return_string(int isPrime) {
-//    return (isPrime==0) ? "is prime": "is not prime";
-//}
-//
-//
-//int is_prime(int num){
-//    int count = 0;
-//    for(int i = 1;i<=num;i++){
-//        if(num%i==0){
-//            count++;
-//        }
-//    }
-//    if(count ==2)
-//        return 0;
-//    else
-//        return 1;
-//}
+
+
+char *return_string(int isPrime) {
+    return (isPrime == 0) ? "is prime" : "is not prime";
+}
+
+
+int is_prime(int num) {
+    int count = 0;
+    for (int i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            count++;
+        }
+    }
+    if (count == 2)
+        return 0;
+    else
+        return 1;
+}
--- composeApp/src/nativeInterop/cinterop/primelib.h
+++ composeApp/src/nativeInterop/cinterop/primelib.h
@@ -1,7 +1,7 @@
 #ifndef LIB2_H_INCLUDED
 #define LIB2_H_INCLUDED
 
-int isPrime(int num);
+int is_prime(int num);
 char* return_string(int isPrime);
 
 #endif
